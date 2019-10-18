a921e9ee5a3e5e11aa36d3b153cf098511720253
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-18 19:48:10 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
-      item = @@items.find{|it| it.name == item_name}
+      item = @@items.find{|item| item.name == item_name}
       if item.nil?
         resp.write "Item not found"
         resp.status = 400
